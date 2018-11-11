dd01e25fdf41d911742d6ba06bdb0098e3c89e72
Saiful Islam <saaifulislam@gmail.com>
2018-11-11 15:03:41 +0600

    Update class-wc-bkash.php

--- includes/class-wc-bkash.php
+++ includes/class-wc-bkash.php
@@ -3,21 +3,24 @@
 /**
  * bKash Class
  */
-class WC_bKash {
+class WC_bKash
+{
 
     const base_url = 'https://www.bkashcluster.com:9081/dreamwave/merchant/trxcheck/sendmsg';
     private $table = 'wc_bkash';
 
-    function __construct() {
-        add_action( 'wp_ajax_wc-bkash-confirm-trx', array($this, 'process_form') );
+    public function __construct()
+    {
+        add_action('wp_ajax_wc-bkash-confirm-trx', array($this, 'process_form'));
 
-        add_action( 'woocommerce_order_details_after_order_table', array($this, 'transaction_form_order_view') );
+        add_action('woocommerce_order_details_after_order_table', array($this, 'transaction_form_order_view'));
     }
 
-    function transaction_form_order_view( $order ) {
+    public function transaction_form_order_view($order)
+    {
 
-        if ( $order->has_status( 'on-hold' ) && $order->payment_method == 'bkash' && ( is_view_order_page() || is_order_received_page() ) ) {
-            self::tranasaction_form( $order->id );
+        if ($order->has_status('on-hold') && $order->payment_method == 'bkash' && (is_view_order_page() || is_order_received_page())) {
+            self::tranasaction_form($order->id);
         }
     }
 
@@ -26,27 +29,28 @@ class WC_bKash {
      *
      * @return void
      */
-    public static function tranasaction_form( $order_id ) {
-        $option = get_option( 'woocommerce_bKash_settings', array() );
+    public static function tranasaction_form($order_id)
+    {
+        $option = get_option('woocommerce_bKash_settings', array());
         ?>
 
         <div class="wc-bkash-form-wrap" style="background: #eee;padding: 15px;border: 1px solid #ddd; margin: 15px 0;">
             <div id="wc-bkash-result"></div>
             <form action="" method="post" id="wc-bkash-confirm" class="wc-bkash-form">
                 <p class="form-row validate-required">
-                    <label><?php _e( 'Transaction ID', 'wc-bkash' ) ?>: <span class="required">*</span></label>
+                    <label><?php _e('Transaction ID', 'wc-bkash')?>: <span class="required">*</span></label>
 
                     <input class="input-text" type="text" name="bkash_trxid" required />
-                    <span class="description"><?php echo isset( $option['trans_help'] ) ? $option['trans_help'] : ''; ?></span>
+                    <span class="description"><?php echo isset($option['trans_help']) ? $option['trans_help'] : ''; ?></span>
                 </p>
 
                 <p class="form-row">
-                    <?php wp_nonce_field( 'wc-bkash-confirm-trx' ); ?>
+                    <?php wp_nonce_field('wc-bkash-confirm-trx');?>
                     <input type="hidden" name="action" value="wc-bkash-confirm-trx">
                     <input type="hidden" name="order_id" value="<?php echo $order_id; ?>">
 
-                    <?php $pay_order_button_text = apply_filters( 'wc_bkash_pay_order_button_text', __( 'Confirm Payment', 'wc-bkash' ) ); ?>
-                    <input type="submit" class="button alt" id="wc-bkash-submit" value="<?php echo esc_attr( $pay_order_button_text ); ?>" />
+                    <?php $pay_order_button_text = apply_filters('wc_bkash_pay_order_button_text', __('Confirm Payment', 'wc-bkash'));?>
+                    <input type="submit" class="button alt" id="wc-bkash-submit" value="<?php echo esc_attr($pay_order_button_text); ?>" />
                 </p>
             </form>
         </div>
@@ -59,7 +63,7 @@ class WC_bKash {
                     var submit = $(this).find('input[type=submit]');
                     submit.attr('disabled', 'disabled');
 
-                    $.post('<?php echo admin_url( 'admin-ajax.php'); ?>', $(this).serialize(), function(data, textStatus, xhr) {
+                    $.post('<?php echo admin_url('admin-ajax.php'); ?>', $(this).serialize(), function(data, textStatus, xhr) {
                         submit.removeAttr('disabled');
 
                         if ( data.success ) {
@@ -72,26 +76,27 @@ class WC_bKash {
             });
         </script>
         <?php
-    }
+}
 
-    public function process_form() {
-        if ( ! wp_verify_nonce( $_POST['_wpnonce'], 'wc-bkash-confirm-trx' ) ) {
-            wp_send_json_error( __( 'Are you cheating?', 'wc-bkash' ) );
+    public function process_form()
+    {
+        if (!wp_verify_nonce($_POST['_wpnonce'], 'wc-bkash-confirm-trx')) {
+            wp_send_json_error(__('Are you cheating?', 'wc-bkash'));
         }
 
-        $order_id       = isset( $_POST['order_id'] ) ? intval( $_POST['order_id'] ) : 0;
-        $transaction_id = sanitize_key( $_POST['bkash_trxid'] );
+        $order_id = isset($_POST['order_id']) ? intval($_POST['order_id']) : 0;
+        $transaction_id = sanitize_key($_POST['bkash_trxid']);
 
-        $order          = wc_get_order( $order_id );
-        $response       = $this->do_request( $transaction_id );
+        $order = wc_get_order($order_id);
+        $response = $this->do_request($transaction_id);
 
-        if ( ! $response ) {
-            wp_send_json_error( __( 'Something went wrong submitting the request', 'wc-bkash' ) );
+        if (!$response) {
+            wp_send_json_error(__('Something went wrong submitting the request', 'wc-bkash'));
             return;
         }
 
-        if ( $this->transaction_exists( $response->trxId ) ) {
-            wp_send_json_error( __('This transaction has already been used!', 'wc-bkash' ) );
+        if ($this->transaction_exists($response->trxId)) {
+            wp_send_json_error(__('This transaction has already been used!', 'wc-bkash'));
             return;
         }
 
@@ -99,57 +104,61 @@ class WC_bKash {
 
             case '0010':
             case '0011':
-                wp_send_json_error( __( 'Transaction is pending, please try again later', 'wc-bkash' ) );
+                wp_send_json_error(__('Transaction is pending, please try again later', 'wc-bkash'));
                 return;
 
             case '0100':
-                wp_send_json_error( __( 'Transaction ID is valid but transaction has been reversed. ', 'wc-bkash' ) );
+                wp_send_json_error(__('Transaction ID is valid but transaction has been reversed. ', 'wc-bkash'));
                 return;
 
             case '0111':
-                wp_send_json_error( __( 'Transaction is failed.', 'wc-bkash' ) );
+                wp_send_json_error(__('Transaction is failed.', 'wc-bkash'));
                 return;
 
             case '1001':
-                wp_send_json_error( __( 'Invalid MSISDN input. Try with correct mobile no.', 'wc-bkash' ) );
+                wp_send_json_error(__('Invalid MSISDN input. Try with correct mobile no.', 'wc-bkash'));
                 break;
 
             case '1002':
-                wp_send_json_error( __( 'Invalid transaction ID', 'wc-bkash' ) );
+                wp_send_json_error(__('Invalid transaction ID', 'wc-bkash'));
                 return;
 
             case '1003':
-                wp_send_json_error( __( 'Authorization Error, please contact site admin.', 'wc-bkash' ) );
+                wp_send_json_error(__('Authorization Error, please contact site admin.', 'wc-bkash'));
                 return;
 
             case '1004':
-                wp_send_json_error( __( 'Transaction ID not found.', 'wc-bkash' ) );
+                wp_send_json_error(__('Transaction ID not found.', 'wc-bkash'));
+                return;
+
+            case '4001':
+                wp_send_json_error(__('Duplicate request. Consecutive hit for the same request within 5 minutes.', 'wc-bkash'));
                 return;
 
             case '9999':
-                wp_send_json_error( __( 'System error, could not process request. Please contact site admin.', 'wc-bkash' ) );
+                wp_send_json_error(__('System error, could not process request. Please contact site admin.', 'wc-bkash'));
                 return;
 
             case '0000':
                 $price = (float) $order->get_total();
 
                 // check for BDT if exists
-                $bdt_price = get_post_meta( $order->id, '_bdt', true );
-                if ( $bdt_price != '' ) {
+                $bdt_price = get_post_meta($order->id, '_bdt', true);
+                if ($bdt_price != '') {
                     $price = $bdt_price;
                 }
 
-                if ( $price > (float) $response->amount ) {
-                    wp_send_json_error( __( 'Transaction amount didn\'t match, are you cheating?', 'wc-bkash' ) );
+                if ($price > (float) $response->amount) {
+                    wp_send_json_error(__('Transaction amount didn\'t match, are you cheating?', 'wc-bkash'));
                     return;
                 }
 
-                $this->insert_transaction( $response );
+                $this->insert_transaction($response);
 
-                $order->add_order_note( sprintf( __( 'bKash payment completed with TrxID#%s! bKash amount: %s', 'wc-bkash' ), $response->trxId, $response->amount ) );
+                $order->add_order_note(sprintf(__('bKash payment completed with TrxID#%s! bKash amount: %s', 'wc-bkash'), $response->trxId, $response->amount));
                 $order->payment_complete();
 
-                wp_send_json_success( $order->get_view_order_url() );
+                wp_send_json_success($order->get_view_order_url());
 
                 break;
         }
@@ -167,22 +176,23 @@ class WC_bKash {
      *
      * @return object
      */
-    function do_request( $transaction_id ) {
+    public function do_request($transaction_id)
+    {
 
-        $option = get_option( 'woocommerce_bKash_settings', array() );
+        $option = get_option('woocommerce_bKash_settings', array());
         $query = array(
-            'user'   => isset( $option['username'] ) ? $option['username'] : '',
-            'pass'   => isset( $option['pass'] ) ? $option['pass'] : '',
-            'msisdn' => isset( $option['mobile'] ) ? $option['mobile'] : '',
-            'trxid'  => $transaction_id
+            'user' => isset($option['username']) ? $option['username'] : '',
+            'pass' => isset($option['pass']) ? $option['pass'] : '',
+            'msisdn' => isset($option['mobile']) ? $option['mobile'] : '',
+            'trxid' => strtoupper($transaction_id),
         );
 
-        $url      = self::base_url . '?' . http_build_query( $query, '', '&' );
-        $response = file_get_contents( $url );
+        $url = self::base_url . '?' . http_build_query($query, '', '&');
+        $response = file_get_contents($url);
 
-        if ( false !== $response ) {
-            $response = json_decode( $response );
-
+        if (false !== $response) {
+            $response = simplexml_load_string($response);
+            $response = json_decode(json_encode($response));
             return $response->transaction;
         }
 
@@ -196,20 +206,21 @@ class WC_bKash {
      *
      * @return void
      */
-    function insert_transaction( $response ) {
+    public function insert_transaction($response)
+    {
         global $wpdb;
 
-        $wpdb->insert( $wpdb->prefix . $this->table, array(
-            'trxId'  => $response->trxId,
+        $wpdb->insert($wpdb->prefix . $this->table, array(
+            'trxId' => $response->trxId,
             'sender' => $response->sender,
-            'ref'    => $response->reference,
-            'amount' => $response->amount
+            'ref' => $response->reference,
+            'amount' => $response->amount,
         ), array(
             '%d',
             '%s',
             '%s',
-            '%s'
-        ) );
+            '%s',
+        ));
     }
 
     /**
@@ -219,13 +230,14 @@ class WC_bKash {
      *
      * @return bool
      */
-    function transaction_exists( $transaction_id ) {
+    public function transaction_exists($transaction_id)
+    {
         global $wpdb;
 
-        $query  = $wpdb->prepare( "SELECT * FROM {$wpdb->prefix}{$this->table} WHERE trxId = %d", $transaction_id );
-        $result = $wpdb->get_row( $query );
+        $query = $wpdb->prepare("SELECT * FROM {$wpdb->prefix}{$this->table} WHERE trxId = %s", $transaction_id);
+        $result = $wpdb->get_row($query);
 
-        if ( $result ) {
+        if ($result) {
             return true;
         }
 
